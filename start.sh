9d6fbdd66d750398c10f0b1da01f1c45a3c00e04
sandeep2042001 <sandeepkumawat246@gmail.com>
2025-09-13 01:18:13 +0530

    Fix Railway deployment configuration and migration issues

--- start.sh
+++ start.sh
@@ -3,6 +3,11 @@
 # Set the port from Railway environment variable
 export PORT=${PORT:-8000}
 
+# Clear caches
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+
 # Run migrations
 php artisan migrate --force
 
